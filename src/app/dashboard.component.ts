bf8c8d2eb764bcef1534e0b30e03c576dde324b2
Tomek Bujakowski <bujakowski@gmail.com>
2017-07-25 23:59:27 +0200

    Add loadState and onSwitchState in Dashboard

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -3,6 +3,13 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html'
+  templateUrl: './dashboard.component.html',
+
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  loadState = 'loading';
+
+  onSwitchState() {
+    this.loadState = 'finished';
+  }
+}
