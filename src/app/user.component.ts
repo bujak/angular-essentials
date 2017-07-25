7e8358de4857ea2837cfdff88c324454927ed4de
Tomek Bujakowski <bujakowski@gmail.com>
2017-07-25 23:27:07 +0200

    Change data binding type in user component

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -4,13 +4,13 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-user',
   template: `
-    <input type="text" (input)="onUserInput($event)" [value]="name"/>
+    <input type="text" [(ngModel)]="name"/>
     <p>Hello {{name}} from user component</p>
   `
   })
 export class UserComponent {
-  name = 'tomek';
-  onUserInput(event) {
-    this.name = event.target.value;
-  }
+  name = '';
+  // onUserInput(event) {
+  //   this.name = event.target.value;
+  // }
 }
