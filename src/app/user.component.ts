4e0218025be606a60175663e711e08ee2f37bc1d
Tomek Bujakowski <bujakowski@gmail.com>
2017-07-25 23:02:35 +0200

    Add value for input in user component

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -4,7 +4,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-user',
   template: `
-    <input type="text" (input)="onUserInput($event)"/>
+    <input type="text" (input)="onUserInput($event)" [value]="name"/>
     <p>Hello {{name}} from user component</p>
   `
   })
