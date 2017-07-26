404f48386da7a16f3e2f97a66a964e4cbae8c82a
Tomek Bujakowski <bujakowski@gmail.com>
2017-07-27 00:14:26 +0200

    Add listener for input event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  rootName = 'TomekApp';
+
+  onNameChanged(newName) {
+    this.rootName = newName;
+  }
 }
