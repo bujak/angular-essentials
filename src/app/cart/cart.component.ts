736a110f5cdcd536f846a783f246fabef2b725a6
Tomek Bujakowski <bujakowski@gmail.com>
2017-08-02 19:08:16 +0200

    Add onAddItem emitter declaration

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-cart',
@@ -7,11 +7,14 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class CartComponent implements OnInit {
   @Input() items = [];
-  newItem = ''
+  newItem = '';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddItem() {
+
+  }
 }
